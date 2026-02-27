913ad79c60cbfcee1c82fc0ae65e937afec2236c
PawełB <134614243+shoogar7@users.noreply.github.com>
2026-02-27 12:49:08 +0100

    fix feature tile bonus; add gitignore, example map, TODO

--- main.py
+++ main.py
@@ -1,49 +1,60 @@
 import random
 
-def generate_map():
-    # features = {"_": 0.7, "~": 0.2, "&": 0.1}
+BOARD_HEIGHT = 50
+BOARD_WIDTH = 100
+            
+def neighbor_bonus(board, i):
+    # defining bonuses so they don't stack up infinitely
+    river_bonus = 1
+    mountain_bonus = 1
     
+    if i > 0:
+        if board[i-1] == "~":
+            river_bonus *= 2
+        elif board[i-1] == "&":
+            mountain_bonus *= 4.5
+    if i >= BOARD_WIDTH:
+        if board[i-BOARD_WIDTH] == "~":
+            river_bonus *= 2.5
+        elif board[i-BOARD_WIDTH] == "&":
+            mountain_bonus *= 3
+        
+    return river_bonus, mountain_bonus
+    
+def generate_map():
     # plain tile | river tile | mountain tile
     features = ("_", "~", "&")
-    weights = [0.8, 0.15, 0.05]
-    
-    # don't generate dictionaries
-    # do all in one line then divide for visuals
-    
-    BOARD_HEIGHT = 50
-    BOARD_WIDTH = 100
-    
-    board_tmp = ""
-    board = ""
+            
+    board_tmp = [] # less mess than with strings
+    board = "" # to write to file
     board_length = BOARD_HEIGHT * BOARD_WIDTH
-    
+            
     for i in range(board_length):
-        multiplier = 1
-        if i > 0:
-            if board_tmp[i-1] in "~&":
-                multiplier *= 1.05
-        if i >= BOARD_WIDTH:
-            if board_tmp[i-BOARD_WIDTH] in "~&":
-                multiplier *= 1.05
-            
-        # apply the multipliers if occured
-        weights[1] *= multiplier 
-        weights[2] *= multiplier
+        # defining here so they don't stack up infinitely
+        weights = [0.78, 0.12, 0.1] # all add up to 1 for simplicity
         
-        # generating board
-        board_tmp += random.choices(features, weights)[0]
-            
+        # calculate the bonuses
+        bonuses = neighbor_bonus(board_tmp, i)
+        
+        weights[1] *= bonuses[0]
+        weights[2] *= bonuses[1]
+                    
+        # draw random tile
+        board_tmp.append(random.choices(features, weights)[0])
+        
+    # convert to string and arrange into board shape
     for i in range(0, board_length, BOARD_WIDTH):
-        board += board_tmp[i:i+BOARD_WIDTH] + "\n"
+        board += "".join(board_tmp[i:i+BOARD_WIDTH]) + "\n"
         
     try: 
         with open("map.txt", "w") as file:
             file.write(board)
     except ValueError:
         print("Wrong file type!")
+    print(weights)
     
 def main():
     generate_map()
     
-if "__name__" == "__main()__":
+if __name__ == "__main__":
     main()
