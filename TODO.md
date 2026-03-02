c0955f10dea146640696f9b55e2cf473a40ba962
PawełB <134614243+shoogar7@users.noreply.github.com>
2026-03-02 22:08:55 +0100

    add feature_cleanup() to clean single-tile features

--- TODO.md
+++ TODO.md
@@ -3,8 +3,8 @@
 What I plan to do with a few thoughts behind some features
 
 - [x] generate terrain
-    - [x] make the generation cooler (like rivers tiles near each other, the same for mountains and if there is a certain amount the probability lessens even more) 
-        - [ ] make the last tile of old line not impact the first tile of new line
-    - [ ] clean up most of the 1-tile land features
+    - [x] implement feature tile bonuses - boost chances of same features near each other
+        - [x] make the last tile of old line not impact the first tile of new line
+    - [x] clean up most of the 1-tile land features
 - [ ] make a loop asking user if he wants to re-generate the map
 - [ ] make a pathfinding algorithm from point A to B
--- main.py
+++ main.py
@@ -1,54 +1,86 @@
 import random
-
+    
 BOARD_HEIGHT = 50
 BOARD_WIDTH = 100
-            
-def neighbor_bonus(board, i):
+    
+def neighbor_bonus(board, y, x):
     # defining bonuses so they don't stack up infinitely
     river_bonus = 1
     mountain_bonus = 1
     
-    if i > 0:
-        if board[i-1] == "~":
+    # check previous tile
+    if x > 0: 
+        if board[y][x-1] == "~":
             river_bonus *= 2
-        elif board[i-1] == "&":
+        elif board[y][x-1] == "&":
             mountain_bonus *= 4.5
-    if i >= BOARD_WIDTH:
-        if board[i-BOARD_WIDTH] == "~":
+    # check tile above
+    if y > 0:
+        if board[y-1][x] == "~":
             river_bonus *= 2.5
-        elif board[i-BOARD_WIDTH] == "&":
+        elif board[y-1][x] == "&":
             mountain_bonus *= 3
         
     return river_bonus, mountain_bonus
+    
+def feature_cleanup(board):
+    new_board = [row[:] for row in board] # make deepcopy of board to not affect original
+        
+    neighbours = [(-1, -1), (0, -1), (1, -1),
+                  (-1, 0),           (1, 0),
+                  (-1, 1), (0, 1), (1, 1)]
+    
+    for y in range(BOARD_HEIGHT):
+        for x in range(BOARD_WIDTH):
+            tile = new_board[y][x]
+            if tile == "_":
+                continue
+            
+            isolated = True
+            
+            for dx, dy in neighbours:
+                nx, ny = x+dx, y+dy
+                if 0 <= nx < BOARD_WIDTH and 0 <= ny < BOARD_HEIGHT:
+                    if new_board[ny][nx] == tile:
+                        isolated = False
+                        break
+                    
+            if isolated:
+                new_board[y][x] = "_"
+                
+    return new_board        
     
 def generate_map():
     # plain tile | river tile | mountain tile
     features = ("_", "~", "&")
             
-    board_tmp = [] # less mess than with strings
-    board = "" # to write to file
-    board_length = BOARD_HEIGHT * BOARD_WIDTH
+    # initializing 2d array
+    board = [[None for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)] 
+    
+    lines = "" # to write to file
             
-    for i in range(board_length):
-        # defining here so they don't stack up infinitely
-        weights = [0.78, 0.12, 0.1] # all add up to 1 for simplicity
+    for y in range(BOARD_HEIGHT):
+        for x in range(BOARD_WIDTH):
+            # defining here so they don't stack up infinitely
+            weights = [0.78, 0.12, 0.1] # all add up to 1 for simplicity
+            
+            # calculate the bonuses
+            bonuses = neighbor_bonus(board, y, x)
+                
+            weights[1] *= bonuses[0]
+            weights[2] *= bonuses[1]
+                        
+            # draw random feature
+            board[y][x] = random.choices(features, weights)[0]
         
-        # calculate the bonuses
-        bonuses = neighbor_bonus(board_tmp, i)
-        
-        weights[1] *= bonuses[0]
-        weights[2] *= bonuses[1]
-                    
-        # draw random tile
-        board_tmp.append(random.choices(features, weights)[0])
-        
-    # convert to string and arrange into board shape
-    for i in range(0, board_length, BOARD_WIDTH):
-        board += "".join(board_tmp[i:i+BOARD_WIDTH]) + "\n"
-        
+    board = feature_cleanup(board)
+    
+    for y in range(BOARD_HEIGHT):
+        lines += "".join(board[y]) + "\n"
+                
     try: 
         with open("map.txt", "w") as file:
-            file.write(board)
+            file.write(lines)
     except ValueError:
         print("Wrong file type!")
     print(weights)
